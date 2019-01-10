3bb1e33572378872275a03a88031cccbd7729fe5
1206497655 <1206497655@qq.com>
2019-01-10 15:42:56 +0800

    add: 编译文件

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
+const path = require('path')
+
 module.exports = {
   // webpack-dev-server 的配置项
   devServer: {
     host: '0.0.0.0',
-    disableHostCheck: true
-  }
+    disableHostCheck: true,
+  },
+  outputDir: 'docs'
+  // configureWebpack: {
+  //   build: {
+  //     // Template for index.html
+  //     index: path.resolve(__dirname, '../docs/index.html'),
+
+  //     // Paths
+  //     assetsRoot: path.resolve(__dirname, '../docs'),
+  //     assetsSubDirectory: 'static',
+  //     assetsPublicPath: './'
+  //   }
+  // }
 }
